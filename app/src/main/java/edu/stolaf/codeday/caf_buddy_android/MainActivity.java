f94292ae0168c00404c7a91b3e3e5c59223cf2e8
a3madkour <a3madkour@gmail.com>
2014-11-09 02:35:00 -0600

    added the signing up and loging in part of the app

--- app/src/main/java/edu/stolaf/codeday/caf_buddy_android/MainActivity.java
+++ app/src/main/java/edu/stolaf/codeday/caf_buddy_android/MainActivity.java
@@ -1,26 +1,76 @@
 package edu.stolaf.codeday.caf_buddy_android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.parse.LogInCallback;
 import com.parse.Parse;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends Activity {
-
+    private EditText username=null;
+    private EditText  password=null;
+    String usernametxt;
+    String passwordtxt;
+    private Button login;
+    private Button signup;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        login = (Button)findViewById(R.id.login);
+        signup = (Button) findViewById(R.id.signup);
         Parse.initialize(this, "svpARfo8bpZIQjUcEspJWfL75lKsUqSDO08DxpYD", "LPpzy5FqoX2SACc9jrL0cZi4CPaNulPmOelFhS2K");
         ParseObject test = new ParseObject("Test");
         test.put("asda","asdas");
         test.saveInBackground();
     }
+    public void login(View view){
+        // Retrieve the text entered from the EditText
+        usernametxt = username.getText().toString();
+        passwordtxt = password.getText().toString();
 
+        // Send data to Parse.com for verification
+        ParseUser.logInInBackground(usernametxt, passwordtxt,
+                new LogInCallback() {
+                    public void done(ParseUser user, ParseException e) {
+                        if (user != null) {
+                            // If user exist and authenticated, send user to Welcome.class
+                           /* Intent intent = new Intent(
+                                    LoginSignupActivity.this,
+                                    Welcome.class);
+                            startActivity(intent);*/
+                            Toast.makeText(getApplicationContext(),
+                                    "Successfully Logged in",
+                                    Toast.LENGTH_LONG).show();
+                            finish();
+                        } else {
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "No such user exist, please signup",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+   public void signup(View view){
 
+       Intent intent = new Intent(this, Sign_up.class);
+       startActivity(intent);
+   }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,3 +93,6 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 }
+
+
+
---
+++ app/src/main/java/edu/stolaf/codeday/caf_buddy_android/Sign_up.java
@@ -0,0 +1,112 @@
+package edu.stolaf.codeday.caf_buddy_android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
+
+public class Sign_up extends Activity {
+    private EditText username=null;
+    private EditText  password=null;
+   private EditText  email=null;
+    String usernametxt;
+    String passwordtxt;
+    String emailtxt;
+    private Button signup;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        username = (EditText)findViewById(R.id.editText1);
+        password = (EditText)findViewById(R.id.editText2);
+        email = (EditText)findViewById(R.id.editText3);
+    }
+    public void Signup(View view){
+        // Retrieve the text entered from the EditText
+        usernametxt = username.getText().toString();
+        passwordtxt = password.getText().toString();
+        emailtxt = email.getText().toString();
+        if (emailtxt.indexOf("@") != -1){
+          if(emailtxt.regionMatches(emailtxt.indexOf("@"),"@stolaf.edu",0,11)== false){
+              Toast.makeText(getApplicationContext(),
+                      "Please enter a valid St. Olaf email 1",
+                      Toast.LENGTH_LONG).show();
+          }
+        }else{
+            Toast.makeText(getApplicationContext(),
+                    "Please enter a valid St. Olaf email 2",
+                    Toast.LENGTH_LONG).show();
+        }
+        // Force user to fill up the form
+        if (usernametxt.equals("") || passwordtxt.equals("") || emailtxt.equals("") ) {
+            Toast.makeText(getApplicationContext(),
+                    "Please complete the sign up form",
+                    Toast.LENGTH_LONG).show();
+
+        } else if(emailtxt.length() <= 11 ){
+            Toast.makeText(getApplicationContext(),
+                    "Please enter a valid St. Olaf email",
+                    Toast.LENGTH_LONG).show();
+        }else if(passwordtxt.length() <=8){
+
+            Toast.makeText(getApplicationContext(),
+                    "Password must be at least 8 characters",
+                    Toast.LENGTH_LONG).show();
+
+        }
+        else {
+            // Save new user data into Parse.com Data Storage
+            ParseUser user = new ParseUser();
+            user.setUsername(usernametxt);
+            user.setPassword(passwordtxt);
+            user.setEmail(emailtxt);
+            user.signUpInBackground(new SignUpCallback() {
+                public void done(ParseException e) {
+                    if (e == null) {
+                        // Show a simple Toast message upon successful registration
+                        Toast.makeText(getApplicationContext(),
+                                "Successfully Signed up, please log in.",
+                                Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(),
+                                "Sign up Error", Toast.LENGTH_LONG)
+                                .show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sign_up, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
